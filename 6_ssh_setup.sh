d48ecb294cce6f7432b40595f5e96ab6f87bc9b7
Alisher Abdulkhaev <alisher.abdulkhaev@gmail.com>
2019-10-11 12:01:06 -0400

    update

--- 6_ssh_setup.sh
+++ 6_ssh_setup.sh
@@ -8,7 +8,7 @@ echo "${BOLD}* SSH setup is started.${NORMAL}";
 apt update && apt install -y openssh-server && rm -rf /var/lib/apt/lists/*
 
 mkdir /var/run/sshd
-echo 'root:elix' | chpasswd
+echo 'root:alisher' | chpasswd
 sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 echo "export VISIBLE=now" >> /etc/profile
