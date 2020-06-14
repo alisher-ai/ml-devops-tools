7f99c73f70cbf81ab9c8082f25bcedafe8ea4887
Alisher Abdulkhaev <alisher.abdulkhaev@gmail.com>
2020-06-15 04:15:19 +0900

    Update file_shuffling_by_line.sh

--- misc/file_shuffling_by_line.sh
+++ misc/file_shuffling_by_line.sh
@@ -1,1 +1,1 @@
-python -c "import random, sys; x = open(sys.argv[1]).readlines(); random.shuffle(x); print ''.join(x)," $1 > $2
+python -c "import random, sys; x = open(sys.argv[1]).readlines(); random.shuffle(x); print(''.join(x))," $1 > $2
